633f47bfe55ed59bc49058024f5c66fa4f516a4f
ahmed <ahmed@gmail.com>
2026-02-03 13:35:30 +0200

    merge

--- src/app/core/components/auth/login/login.component.ts
+++ src/app/core/components/auth/login/login.component.ts
@@ -10,45 +10,45 @@ import { Router } from '@angular/router';
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
-private authService: AuthService= inject(AuthService);
-private router:Router=inject(Router);
-errMsg :WritableSignal<string>=signal<string>('');
-isLoading:WritableSignal<boolean>=signal<boolean>(false);
-logInForm:FormGroup = new FormGroup({
-email:new FormControl(null,[Validators.required,Validators.email]),
-password:new FormControl(null,[Validators.required,Validators.pattern(/^[A-Z][a-z0-9]{7,20}$/)]),
-}) 
-submitLoginForm(){
-  
-if(this.logInForm.valid){
-this.isLoading.set(true);
-this.authService.logIn(this.logInForm.value).subscribe({
+  private authService: AuthService = inject(AuthService);
+  private router: Router = inject(Router);
+  errMsg: WritableSignal<string> = signal<string>('');
+  isLoading: WritableSignal<boolean> = signal<boolean>(false);
+  logInForm: FormGroup = new FormGroup({
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    password: new FormControl(null, [Validators.required, Validators.pattern(/^[A-Z][a-z0-9]{7,20}$/)]),
+  })
+  submitLoginForm() {
 
-next: res =>{
-console.log(res);
-this.isLoading.set(false);
-localStorage.setItem('userToken',res.token)
-this.authService.decodeUserData();
-this.router.navigate(['home'])
-},
-error: err=>{
-this.errMsg.set(err.error.message);
-this.isLoading.set(false);
-console.log(this.errMsg());
+    if (this.logInForm.valid) {
+      this.isLoading.set(true);
+      this.authService.logIn(this.logInForm.value).subscribe({
 
-
-}
+        next: res => {
+          console.log(res);
+          this.isLoading.set(false);
+          localStorage.setItem('userToken', res.token)
+          this.authService.decodeUserData();
+          this.router.navigate(['home'])
+        },
+        error: err => {
+          this.errMsg.set(err.error.message);
+          this.isLoading.set(false);
+          console.log(this.errMsg());
 
 
+        }
 
 
 
 
-})
 
 
-}
+      })
 
-}
+
+    }
+
+  }
 
 }
